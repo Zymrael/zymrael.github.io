5ea9cccaa6903331ab7c7b60cee0b50af1935beb
Zymrael <mic.poli@hotmail.it>
2022-06-09 23:23:13 -0700

    self-sim website is go

--- self-similarity-prior/static/js/index.js
+++ self-similarity-prior/static/js/index.js
@@ -39,24 +39,8 @@ $(document).ready(function() {
     }
 
 		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
+    var defaultCarousels = bulmaCarousel.attach('.carousel', options);
+    var twoSlideCarousels = bulmaCarousel.attach('.two-slide-carousel', { ...options, slidesToShow: 2, loop: false, infinite: false});
 
     /*var player = document.getElementById('interpolation-video');
     player.addEventListener('loadedmetadata', function() {
